67467e1e3e6b2f38db48d4374b504d0e5da34616
Emerald Greenlight <egreenlight.pav@gmail.com>
2016-11-17 15:56:35 +0300

    Removed unneeded steamid package

--- app/index.js
+++ app/index.js
@@ -2,7 +2,6 @@
 
 let express = require('express'),
     http = require('http'),
-    converter = require('steamid'),
     CSteamCommunity = require('steamcommunity');
 
 let SteamCommunity = new CSteamCommunity();
@@ -55,22 +54,21 @@ app.get('/api/arbitraryStringToSteamId', function(req, res, next) {
         if (error) {
             res.status(500).send({'error' : error})
         } else {
-		let id64 = data.steamID.toString(); // was used in 2 places already, needs a shorthand
-		let tonsOfData = new converter(id64); // using steamid package for now, may be rewritten to get rid of it later
+		let theID = data.steamID; // using steamid package for now, may be rewritten to get rid of it later
             res.send({
                 'name' : data.name,
-		'Steam Universe' : tonsOfData.universe,
-		'Account Type' : tonsOfData.type,
-		'Account Instance' : tonsOfData.instance,
-		'steam2-old' : tonsOfData.getSteam2RenderedID(),
-		'steam2-new' : tonsOfData.getSteam2RenderedID(true),
-		'steamId3' : tonsOfData.getSteam3RenderedID(),
-		'steam3-accountid' : tonsOfData.accountid,
-                'steamId64' : id64,
+		'Steam Universe' : theID.universe,
+		'Account Type' : theID.type,
+		'Account Instance' : theID.instance,
+		'steam2-old' : theID.getSteam2RenderedID(),
+		'steam2-new' : theID.getSteam2RenderedID(true),
+		'steamId3' : theID.getSteam3RenderedID(),
+		'steam3-accountid' : theID.accountid,
+                'steamId64' : theID.toString(),
                 'customUrl' : data.customURL,
-		'Profile URL' : 'http://steamcommunity.com/profiles/' + id64,
-		'is groupchat?' : tonsOfData.isGroupChat(),
-		'is Steam Lobby?' : tonsOfData.isLobby(),
+		'Profile URL' : 'http://steamcommunity.com/profiles/' + theID.toString(),
+		'is groupchat?' : theID.isGroupChat(),
+		'is Steam Lobby?' : theID.isLobby(),
                 'memberSince' : data.memberSince,
                 'online?' : data.onlineState
             }); //TODO: more fields here
