2780e267210abf89b58c4bba58a28324867a9328
Roman Kuzmichev <Daerdemandt@gmail.com>
2016-11-17 16:20:59 +0300

    Add tring other ID formats too

--- app/index.js
+++ app/index.js
@@ -25,12 +25,15 @@ let urlToSteamId = function (url) {};
 
 let stringToSteamId = function(string, cb) {
     const notFound = 'Error: The specified profile could not be found.';
-    let tryAsId = function () {
+    let tryAsId = function (string, cb) {
         try {
+            console.log('trying ' + string);
             let id = new CSteamCommunity.SteamID(string);
             SteamCommunity.getSteamUser(id, cb);
         } catch(error) {
-            if (error.stack.startsWith('Error: Unknown SteamID input format')) {
+            if (error.stack.startsWith('Error: SteamID must stand for an individual account in the public')) {
+                cb(null, null); // not found
+            } else if (error.stack.startsWith('Error: Unknown SteamID input format')) {
                 cb('Unsupported format, but we are working on it'); //TODO: support URLs
             } else {
                 cb(error);
@@ -43,17 +46,40 @@ let stringToSteamId = function(string, cb) {
             cb(null, result);
         } else {
             if (err.stack.startsWith(notFound)) {
-                tryAsId();
+                let asInt = parseInt(string);
+                if (asInt) {
+                    let asOld = `STEAM_0:0:${asInt}`;
+                    let asV3 = `[U:1:${asInt}]`;
+                    tryUntilResult([string, asOld, asV3], tryAsId, cb, null);
+                }
             }
         }
     });
 };
-
-
+///*
+let tryUntilResult = function(items, process, successCallback, stubData) {
+    if (0 == items.length) {
+        successCallback(stubData);
+    } else {
+        //let [current, rest] = [items[0], items.slice(1)];
+        let current = items[0];
+        let rest = items.slice(1);
+        process(current, function(err, data) {
+            if (err || data) {
+                successCallback(err, data);
+            } else { // no errors, but we need to try other options
+                tryUntilResult(rest, process, successCallback, stubData);
+            }
+        });
+    }
+}
+//*/
 app.get('/api/arbitraryStringToSteamId', function(req, res, next) {
     let ret = function(error, data) {
         if (error) {
-            res.status(500).send({'error' : error})
+            res.status(500).send({'error' : error.stack})
+        } else if (!data) {
+            res.status(404).send({'error' : 'not found'});
         } else {
 		let id64 = data.steamID.toString(); // was used in 2 places already, needs a shorthand
 		let tonsOfData = new converter(id64); // using steamid package for now, may be rewritten to get rid of it later
