f6ffc54365a6c88eff510db2d7a26b1b98dfc838
Roman Kuzmichev <Daerdemandt@gmail.com>
2017-01-06 00:40:11 +0300

    Minor refactoring, more tests

--- app/index.js
+++ app/index.js
@@ -14,7 +14,7 @@ let opportunisticProcessing = function(options, callback) {
 	if (!options.length) {
 		return (data) => callback({error: `No methods were able to process '${data}'`});
 	} else {
-        let [first, rest] = [options[0], options.slice(1)];
+        let [first, ...rest] = options;
 		return (data) => first(null, data, function(err, result) {
 			if (!err) {
 				return callback(null, result);
@@ -74,7 +74,7 @@ let urlToSteamId = function (url) {
 };
 
 
-let methods = {
+const methods = {
 	'asId64'	: nameOutput('id64', mutateInput(onlyIfNumeric(), toSteamUser)),
 	'asId3Tail'	: nameOutput('id3', mutateInput(
 		onlyIfNumeric((string) => toSteamId(`[U:1:${string}]`)),
@@ -88,8 +88,13 @@ let methods = {
 	'asName'	: nameOutput('name', toSteamUser),
 	'asUrl'		: nameOutput('url', mutateInput(urlToSteamId, toSteamUser))
 };
+const methodSets = {
+	'default' : ['asId64', 'asName', 'asValidId', 'asId3Tail', 'asOldTail', 'asUrl'],
+	'oldFirst' : ['asOldTail', 'asId64', 'asName', 'asValidId', 'asId3Tail', 'asUrl']
+};
 
-let resolveSteamUser = function(string, methodsToTry, cb) {
+
+const resolveSteamUser = function(string, methodsToTry, cb) {
 	opportunisticProcessing(methodsToTry.map((name) => methods[name]), cb)(string);
 }
 
@@ -100,7 +105,9 @@ const testsSure = {
     'customUrlPiece' : 'daerdemandt',
     'name' : 'Daerdemandt',
     'customUrl' : 'http://steamcommunity.com/id/daerdemandt',
-    'profileUrl' : 'http://steamcommunity.com/profiles/76561198083147756'
+    'profileUrl' : 'http://steamcommunity.com/profiles/76561198083147756',
+    'customUrlDirty' : 'http://steamcommunity.com/id/daerdemandt/top#kek',
+    'profileUrlDirty' : 'http://steamcommunity.com/profiles/76561198083147756/top#kek'
 };
 const testsAmbiguous = {
 	'ID3tail' : {data:'122882028', method:'asId3Tail'},
@@ -113,19 +120,13 @@ const runTest = function(name, data, methods) {
 			console.log(`Startup test failed for ${name}`);
 			console.log(err || `'${result.value.name}' is not Daerdemandt`);
 			process.exit();
-		} else {console.log(`Sanity check successful at ${name} : ${result.type}`)}
+		} //else {console.log(`Sanity check successful at ${name} : ${result.type}`)}
 	});
 }
+Object.keys(testsSure).forEach(name => runTest(name, testsSure[name], methodSets.default));
+Object.keys(testsAmbiguous).forEach((name) => runTest(name, testsAmbiguous[name].data, [testsAmbiguous[name].method]));
 
-for (const testCase in testsSure) {
-	//const methods = ['asId64', 'asId3Tail', 'asOldTail', 'asValidId', 'asUrl'];
-	const methods = ['asId64', 'asValidId', 'asName', 'asUrl', 'asId3Tail', 'asOldTail'];
-	runTest(testCase, testsSure[testCase], methods);
-}
-for (const name in testsAmbiguous) {
-	runTest(name, testsAmbiguous[name].data, [testsAmbiguous[name].method]);
-}
-
+const defaultUserSettings = {'methodSet':'default', 'methods':[], 'autocopy':null};
 
 let app = express();
 app.use(cookieParser());
@@ -156,14 +157,8 @@ app.get('/api/arbitraryStringToSteamId', function(req, res, next) {
             }); //TODO: more fields here
         }
     };
-	let defaultSettings = {'methodSet':'default', 'methods':[], 'autocopy':null},
-		settings = cookieParser.JSONCookie(req.cookies.settings) || defaultSettings,
-		methodSets = {
-			'default' : ['asId64', 'asId3Tail', 'asOldTail', 'asValidId', 'asUrl'],
-			'oldFirst' : ['asOldTail', 'asId64', 'asId3Tail', 'asValidId', 'asUrl']
-		};
-
-	let methodsToTry = [],
+	let settings = cookieParser.JSONCookie(req.cookies.settings) || defaultUserSettings,
+		methodsToTry = [],
 		query = decodeURIComponent(req.query.string.trim());
 
 	if (settings.methods && settings.methods.length) {
@@ -177,14 +172,6 @@ app.get('/api/arbitraryStringToSteamId', function(req, res, next) {
 app.get('/', function(req, res, next) {
     res.sendFile( '/static/' + 'index.html');
 });
-
-function processConvertQuery(request, response)
-{
-	response.status(501).send("STUB!");
-}
-
-
-app.post('/api/convert', processConvertQuery);
 
 http.createServer(app).listen(process.env.PORT || 8080, function() {
     console.log('Listening on port ' + (process.env.PORT || 8080));
